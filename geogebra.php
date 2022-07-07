2f39fa159f7e681f7d39440ebbf7d267a090a624
Takesomen <67959492+Takesomen@users.noreply.github.com>
2022-07-07 18:53:55 +0300

    07.07.2022

--- geogebra.php
+++ geogebra.php
@@ -20,50 +20,30 @@ echo $OUTPUT->header();
 ?>
   <script src="https://www.geogebra.org/apps/deployggb.js"></script>
   <script>
-    console.log(tinymce)
-    // var str = window.tinyMCE.activeEditor.getContent();
-    // var appName = 'graphig';
-    // var begin = str.indexOf('ggbBase64')
-  
-    // if (begin != -1) {
-    //   begin = str.indexOf('ggbBase64')+11;
-    //   var end = str.indexOf("=='};")+2;
-    //   appName = str.slice(str.indexOf('appName')+10,str.indexOf('width')-3);
-    //   var code64 = str.slice(begin, end);
-    //   var params = {
-    //   appName: `'${appName}'`, 
-    //   width: window.screen.width - 100,
-    //   height: window.screen.height - 150, 
-    //   showToolBar: true, 
-    //   showAlgebraInput: true, 
-    //   showMenuBar: true,
-    //   ggbBase64: `'${code64}'`,
-    //   };
-    // }
-    // else {
-      var params = {
-      // appName: `'${appName}'`, 
+    var params = {
       appName: 'graphig', 
-      width: window.screen.width - 100,
-      height: window.screen.height - 150, 
+      width: window.outerWidth,
+      height: window.outerHeight - 250, 
       showToolBar: true, 
       showAlgebraInput: true, 
       showMenuBar: true,
-      };
-    // }
+    };
     var applet = new GGBApplet(params, true);
     window.onload = function () {applet.inject('ggb-element');};
   </script>
   
 <div id="ggb-element">
 </div>
-<div>
-  <input type="button" id="graphig" name="graphig" value="graphig" onclick="GeoDialog.reloadGraphig();"/>
-  <input type="button" id="geometry" name="geometry" value="geometry" onclick="GeoDialog.reloadGeometry();"/>
-  <input type="button" id="3d" name="3d" value="3d" onclick="GeoDialog.reload3d();"/>
-  <input type="button" id="insert" name="insert" value="Вставить" onclick="GeoDialog.insert();"/>
-  <input type="button" id="cancel" name="cancel" value="Отмена" onclick="tinyMCEPopup.close();"/> 
-</div>   
+<p>
+  <div>
+    <input type="button" id="graphig" name="graphig" value="График" onclick="GeoDialog.reloadGraphig();"/>
+    <input type="button" id="geometry" name="geometry" value="Геометрия" onclick="GeoDialog.reloadGeometry();"/>
+    <input type="button" id="3d" name="3d" value="3д" onclick="GeoDialog.reload3d();"/>
+    <input type="button" id="editing" name="editing" value="Редактировать" onclick="GeoDialog.loadLastGGB();"/>
+    <input type="button" id="insert" name="insert" value="Вставить" onclick="GeoDialog.insert();"/>
+    <input type="button" id="cancel" name="cancel" value="Отмена" onclick="tinyMCEPopup.close();"/>
+  </div>
+</p>  
 <?php
 
 echo $OUTPUT->footer();
